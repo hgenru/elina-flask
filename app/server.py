9fc3831b0365db06a52260ead35505380fc6ef12
Elina Kirillova <elinakirillov@gmail.com>
2017-05-11 13:59:14 +0300

    Improve code after Sasha's comments

--- app/server.py
+++ app/server.py
@@ -3,18 +3,18 @@ from flask import Flask, render_template, request, redirect
 
 application = Flask('elinas_app')
 
-@application.route("/", methods=['GET','POST'])
+@application.route("/", methods=['POST'])
 def input_data():
 	return render_template('form.html')
 
 
-@application.route("/calculate", methods=['GET','POST'])
+@application.route("/calculate", methods=['GET'])
 def calculate():
 	number_one = request.form.get('number_one')
 	number_two = request.form.get('number_two')
 	number_one = float(number_one)
 	number_two = float(number_two)
-	sum_one_two = int(number_one + number_two)
+	sum_one_two = float(number_one + number_two)
 	return render_template ('result.html', sum_one_two=sum_one_two)
 
 if __name__ == "__main__":
