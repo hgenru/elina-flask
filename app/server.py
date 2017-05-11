33293679be84adf15466aea7090e63effbec7294
Elina Kirillova <elinakirillov@gmail.com>
2017-05-11 15:00:42 +0300

    Little modificarion for working version

--- app/server.py
+++ app/server.py
@@ -8,13 +8,11 @@ def input_data():
 	return render_template('form.html')
 
 
-@application.route("/calculate", methods=['GET'])
+@application.route("/calculate", methods=['GET','POST'])
 def calculate():
-	number_one = request.form.get('number_one')
-	number_two = request.form.get('number_two')
-	number_one = float(number_one)
-	number_two = float(number_two)
-	sum_one_two = float(number_one + number_two)
+	number_one = int(request.form.get('number_one'))
+	number_two = int(request.form.get('number_two'))
+	sum_one_two = int(number_one + number_two)
 	return render_template ('result.html', sum_one_two=sum_one_two)
 
 if __name__ == "__main__":
