d09ea888421daa94034a2e8f067b6e5c3fdc2d93
Elina Kirillova <elinakirillov@gmail.com>
2017-05-11 21:08:39 +0300

    Succesfully finish 3 tests

--- app/server.py
+++ app/server.py
@@ -3,15 +3,20 @@ from flask import Flask, render_template, request, redirect
 
 application = Flask('elinas_app')
 
-@application.route("/", methods=['POST'])
+#application - это декоратор (модификатор функции, функция для функции)
+#синтаксический сакр - одной функции передала другой функции
+#в декораторе route функция начала обрабатывать урлы
+@application.route("/", methods=['GET'])
 def input_data():
 	return render_template('form.html')
 
-
-@application.route("/calculate", methods=['GET','POST'])
+#запрос GET - «Дай мне что-то». POST — «Создай что-то»
+#Постом обязательно делаются конфид данные (например, пароли или сообщения
+#чтобы не видно было в урле)
+@application.route("/calculate", methods=['POST'])
 def calculate():
-	number_one = int(request.form.get('number_one'))
-	number_two = int(request.form.get('number_two'))
+	number_one = int(request.form.get("number_one"))
+	number_two = int(request.form.get("number_two"))
 	sum_one_two = int(number_one + number_two)
 	return render_template ('result.html', sum_one_two=sum_one_two)
 
