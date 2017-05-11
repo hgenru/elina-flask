acd8b8f84cf3add3e11477ba7977db10c5cab9bf
Elina Kirillova <elinakirillov@gmail.com>
2017-05-11 13:38:23 +0300

    Finish 3rd test

--- app/server.py
+++ app/server.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 application = Flask('elinas_app')
 
-@application.route("/")
-def hello():
+@application.route("/", methods=['GET','POST'])
+def input_data():
+	number_one = request.form.get('number_one')
+	number_two = request.form.get('number_two')
+	
 	return render_template('form.html')
 
+
+@application.route("/calculate", methods=['GET','POST'])
+def calculate():
+	number_one = request.form.get('number_one')
+	number_two = request.form.get('number_two')
+	number_one = float(number_one)
+	number_two = float(number_two)
+	sum_one_two = int(number_one + number_two)
+	return render_template ('result.html', sum_one_two=sum_one_two)
+
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
